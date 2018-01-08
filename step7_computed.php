8983983496c3192832ec24083d57a28dec732d80
doshisneha <doshisneha64@gmail.com>
2018-01-08 00:08:20 -0800

    COMPUTED

--- step7_computed.php
+++ step7_computed.php
@@ -11,20 +11,64 @@
   <div id="app">
      <p>Original message: "{{ message }}"</p>
      <p>Computed reversed message: "{{ reversedMessage }}"</p>
+
+    <h4>All Task</h4>
+     <ul >
+       <li v-for="task in tasks" v-text="task.description"></li>
+     </ul>
+
+
+
+     <h4>Computed  Task</h4>
+     <ul >
+       <li v-for="task in tasks" v-if="task.computed" v-text="task.description"></li>
+     </ul>
+     OR
+
+     <ul >
+       <li v-for="task in completetasks"  v-text="task.description"></li>
+     </ul>
+
+     <h4>Incomplete Task</h4>
+
+     <ul >
+       <li v-for="task in incompletetasks"  v-text="task.description"></li>
+     </ul>
+
+
   </div>
   <script>
     var app = new Vue({
       el: '#app',
       data: {
-        message: 'Hello'
+        message: 'Hello',
+        tasks:[
+          {description:'Hello World',computed:true,},
+          {description:'How R U',computed:false,},
+          {description:'Fine Thank U',computed:false,},
+          {description:'And U?',computed:true,},
+
+        ]
       },
       computed: {
             // a computed getter
             reversedMessage: function () 
             {
              // `this` points to the vm instance
-            return this.message.split('').reverse().join('')
+             return this.message.split('').reverse().join('');
+            },
+
+             
+           incompletetasks() 
+             {
+               return this.tasks.filter(task => ! task.computed);
+             } ,
+
+             completetasks() 
+             {
+               return this.tasks.filter(task => task.computed);
              }
+
          }
     })
 
